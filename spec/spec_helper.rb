f1d1c790fbb448c12547c65797453b708013f484
Tobias Schmidt <ts@soundcloud.com>
2014-05-15 19:26:31 -0400

    Add coveralls support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,13 @@
 # encoding: UTF-8
 
 require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter =
+  if ENV['CI']
+    Coveralls::SimpleCov::Formatter
+  else
+    SimpleCov::Formatter::HTMLFormatter
+  end
+
 SimpleCov.start
