c81b438625e834e85fec40f1733d55b3b38ba618
Tobias Schmidt <ts@soundcloud.com>
2016-01-11 19:23:18 -0500

    Remove JSON exposition format support
    JSON has been deprecated as exposition format long time ago. Time to
    remove support from client_ruby.

--- spec/prometheus/client/rack/exporter_spec.rb
+++ spec/prometheus/client/rack/exporter_spec.rb
@@ -1,6 +1,5 @@
 # encoding: UTF-8
 
-require 'json'
 require 'rack/test'
 require 'prometheus/client/rack/exporter'
 
@@ -27,7 +26,6 @@ describe Prometheus::Client::Rack::Exporter do
 
   context 'when requesting /metrics' do
     text = Prometheus::Client::Formats::Text
-    json = Prometheus::Client::Formats::JSON
 
     shared_examples 'ok' do |headers, fmt|
       it "responds with 200 OK and Content-Type #{fmt::CONTENT_TYPE}" do
@@ -43,7 +41,7 @@ describe Prometheus::Client::Rack::Exporter do
 
     shared_examples 'not acceptable' do |headers|
       it 'responds with 406 Not Acceptable' do
-        message = 'Supported media types: text/plain, application/json'
+        message = 'Supported media types: text/plain'
 
         get '/metrics', nil, headers
 
@@ -54,37 +52,19 @@ describe Prometheus::Client::Rack::Exporter do
     end
 
     context 'when client does not send a Accept header' do
-      include_examples 'ok', {}, json
+      include_examples 'ok', {}, text
     end
 
     context 'when client accpets any media type' do
-      accept = '*/*'
-
-      include_examples 'ok', { 'HTTP_ACCEPT' => accept }, json
+      include_examples 'ok', { 'HTTP_ACCEPT' => '*/*' }, text
     end
 
     context 'when client requests application/json' do
-      accept = 'application/json'
-
-      include_examples 'ok', { 'HTTP_ACCEPT' => accept }, json
-    end
-
-    context "when client requests '#{json::CONTENT_TYPE}'" do
-      accept = json::CONTENT_TYPE
-
-      include_examples 'ok', { 'HTTP_ACCEPT' => accept }, json
+      include_examples 'not acceptable', 'HTTP_ACCEPT' => 'application/json'
     end
 
     context 'when client requests text/plain' do
-      accept = 'text/plain'
-
-      include_examples 'ok', { 'HTTP_ACCEPT' => accept }, text
-    end
-
-    context "when client requests '#{text::CONTENT_TYPE}'" do
-      accept = text::CONTENT_TYPE
-
-      include_examples 'ok', { 'HTTP_ACCEPT' => accept }, text
+      include_examples 'ok', { 'HTTP_ACCEPT' => 'text/plain' }, text
     end
 
     context 'when client uses different white spaces in Accept header' do
@@ -93,20 +73,14 @@ describe Prometheus::Client::Rack::Exporter do
       include_examples 'ok', { 'HTTP_ACCEPT' => accept }, text
     end
 
-    context 'when client accepts multiple formats' do
-      accept = "#{json::CONTENT_TYPE};q=0.5, #{text::CONTENT_TYPE};q=0.7"
-
-      include_examples 'ok', { 'HTTP_ACCEPT' => accept }, text
-    end
-
     context 'when client does not include quality attribute' do
-      accept = "#{json::CONTENT_TYPE};q=0.5, #{text::CONTENT_TYPE}"
+      accept = 'application/json;q=0.5, text/plain'
 
       include_examples 'ok', { 'HTTP_ACCEPT' => accept }, text
     end
 
     context 'when client accepts some unknown formats' do
-      accept = "#{text::CONTENT_TYPE};q=0.3, proto/buf;q=0.7"
+      accept = 'text/plain;q=0.3, proto/buf;q=0.7'
 
       include_examples 'ok', { 'HTTP_ACCEPT' => accept }, text
     end
@@ -120,7 +94,7 @@ describe Prometheus::Client::Rack::Exporter do
     context 'when client accepts unknown formats and wildcard' do
       accept = 'fancy/woo;q=0.3, proto/buf;q=0.7, */*;q=0.1'
 
-      include_examples 'ok', { 'HTTP_ACCEPT' => accept }, json
+      include_examples 'ok', { 'HTTP_ACCEPT' => accept }, text
     end
   end
 end
