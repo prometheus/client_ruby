e7a22f689d3f7da99ab0543d9518673c0f67609a
Christoph Olszowka <christoph@olszowka.de>
2016-11-21 12:49:59 +0100

    Add possibility to talk HTTPS in push client

--- spec/prometheus/client/push_spec.rb
+++ spec/prometheus/client/push_spec.rb
@@ -52,7 +52,7 @@ describe Prometheus::Client::Push do
   end
 
   describe '#add' do
-    it 'pushes a given registry to the configured Pushgateway' do
+    it 'pushes a given registry to the configured Pushgateway via HTTP' do
       http = double(:http)
       expect(http).to receive(:send_request).with(
         'POST',
@@ -60,9 +60,24 @@ describe Prometheus::Client::Push do
         Prometheus::Client::Formats::Text.marshal(registry),
         'Content-Type' => Prometheus::Client::Formats::Text::CONTENT_TYPE,
       )
+      expect(http).to receive(:use_ssl=).with(false)
       expect(Net::HTTP).to receive(:new).with('pu.sh', 9091).and_return(http)
 
       described_class.new('foo', 'bar', 'http://pu.sh:9091').add(registry)
+    end
+
+    it 'pushes a given registry to the configured Pushgateway via HTTPS' do
+      http = double(:http)
+      expect(http).to receive(:send_request).with(
+        'POST',
+        '/metrics/jobs/foo/instances/bar',
+        Prometheus::Client::Formats::Text.marshal(registry),
+        'Content-Type' => Prometheus::Client::Formats::Text::CONTENT_TYPE,
+      )
+      expect(http).to receive(:use_ssl=).with(true)
+      expect(Net::HTTP).to receive(:new).with('pu.sh', 9091).and_return(http)
+
+      described_class.new('foo', 'bar', 'https://pu.sh:9091').add(registry)
     end
   end
 
@@ -75,6 +90,7 @@ describe Prometheus::Client::Push do
         Prometheus::Client::Formats::Text.marshal(registry),
         'Content-Type' => Prometheus::Client::Formats::Text::CONTENT_TYPE,
       )
+      expect(http).to receive(:use_ssl=).with(false)
       expect(Net::HTTP).to receive(:new).with('pu.sh', 9091).and_return(http)
 
       described_class.new('foo', 'bar', 'http://pu.sh:9091').replace(registry)
@@ -88,6 +104,7 @@ describe Prometheus::Client::Push do
         'DELETE',
         '/metrics/jobs/foo/instances/bar',
       )
+      expect(http).to receive(:use_ssl=).with(false)
       expect(Net::HTTP).to receive(:new).with('pu.sh', 9091).and_return(http)
 
       described_class.new('foo', 'bar', 'http://pu.sh:9091').delete
