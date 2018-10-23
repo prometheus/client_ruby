3178104710a912f282d0b13122483cb4d555a65f
Daniel Magliola <danielmagliola@gocardless.com>
2018-10-23 17:43:26 +0100

    Add `DataStores::SingleThreaded`
    This is the simplest possible store we can have, which simply has a hash
    for each metric, and no synchronization code whatsoever.
    
    It can only be used in single-threaded, single-process scenarios, but for
    those, it should be the fastest option.
    
    Signed-off-by: Daniel Magliola <danielmagliola@gocardless.com>

---
+++ lib/prometheus/client/data_stores/single_threaded.rb
@@ -0,0 +1,58 @@
+require 'concurrent'
+
+module Prometheus
+  module Client
+    module DataStores
+      # Stores all the data in a simple Hash for each Metric
+      #
+      # Has *no* synchronization primitives, making it the fastest store for single-threaded
+      # scenarios, but must absolutely not be used in multi-threaded scenarios.
+      class SingleThreaded
+        class InvalidStoreSettingsError < StandardError; end
+
+        def for_metric(metric_name, metric_type:, metric_settings: {})
+          # We don't need `metric_type` or `metric_settings` for this particular store
+          validate_metric_settings(metric_settings: metric_settings)
+          MetricStore.new
+        end
+
+        private
+
+        def validate_metric_settings(metric_settings:)
+          unless metric_settings.empty?
+            raise InvalidStoreSettingsError,
+                  "SingleThreaded doesn't allow any metric_settings"
+          end
+        end
+
+        class MetricStore
+          def initialize
+            @internal_store = Hash.new { |hash, key| hash[key] = 0.0 }
+          end
+
+          def synchronize
+            yield
+          end
+
+          def set(labels:, val:)
+            @internal_store[labels] = val.to_f
+          end
+
+          def increment(labels:, by: 1)
+            @internal_store[labels] += by
+          end
+
+          def get(labels:)
+            @internal_store[labels]
+          end
+
+          def all_values
+            @internal_store.dup
+          end
+        end
+
+        private_constant :MetricStore
+      end
+    end
+  end
+end
