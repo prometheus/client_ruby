gocardless/prometheus_client_ruby

Change `text_spec.rb` to be more like an integration test
The existing spec on `Formats::Text` is based on mocking a registry with
fake metrics and values inside, which means if anything changes in the
interface for metrics, the test will not catch it.

And there's no test validating that interface.

This test is more realistic, and it actually catches the kind of bugs
we introduced in the process of this refactor

spec/prometheus/client/formats/text_spec.rb
@@ -1,62 +1,53 @@
 # encoding: UTF-8
 
+require 'prometheus/client/registry'
 require 'prometheus/client/formats/text'
 
 describe Prometheus::Client::Formats::Text do
-  let(:summary_value) do
-    { "count" => 93.0, "sum" => 1243.21 }
+  # Reset the data store
+  before do
+    Prometheus::Client.config.data_store = Prometheus::Client::DataStores::SimpleHash.new
   end
 
-  let(:histogram_value) do
-    { 10 => 1.0, 20 => 2.0, 30 => 2.0, "+Inf" => 2.0, "sum" => 15.2 }
-  end
+  let(:registry) { Prometheus::Client::Registry.new }
 
-  let(:registry) do
-    metrics = [
-      double(
-        name: :foo,
-        docstring: 'foo description',
-        type: :counter,
-        values: {
-          { umlauts: 'Björn', utf: '佖佥', code: 'red' }   => 42.0,
-          { umlauts: 'Björn', utf: '佖佥', code: 'green' } => 3.14E42,
-          { umlauts: 'Björn', utf: '佖佥', code: 'blue' }  => -1.23e-45,
-        },
-      ),
-      double(
-        name: :bar,
-        docstring: "bar description\nwith newline",
-        type: :gauge,
-        values: {
-          { status: 'success', code: 'pink' } => 15.0,
-        },
-      ),
-      double(
-        name: :baz,
-        docstring: 'baz "description" \\escaping',
-        type: :counter,
-        values: {
-          { text: "with \"quotes\", \\escape \n and newline" } => 15.0,
-        },
-      ),
-      double(
-        name: :qux,
-        docstring: 'qux description',
-        type: :summary,
-        values: {
-          { for: 'sake', code: '1' } => summary_value,
-        },
-      ),
-      double(
-        name: :xuq,
-        docstring: 'xuq description',
-        type: :histogram,
-        values: {
-          { code: 'ah' } => histogram_value,
-        },
-      ),
-    ]
-    double(metrics: metrics)
+  before do
+    foo = registry.counter(:foo,
+                           docstring: 'foo description',
+                           labels: [:umlauts, :utf, :code],
+                           preset_labels: {umlauts: 'Björn', utf: '佖佥'})
+    foo.increment(labels: { code: 'red'}, by: 42)
+    foo.increment(labels: { code: 'green'}, by: 3.14E42)
+    foo.increment(labels: { code: 'blue'}, by: 1.23e-45)
+
+
+    bar = registry.gauge(:bar,
+                         docstring: "bar description\nwith newline",
+                         labels: [:status, :code])
+    bar.set(15, labels: { status: 'success', code: 'pink'})
+
+
+    baz = registry.counter(:baz,
+                           docstring: 'baz "description" \\escaping',
+                           labels: [:text])
+    baz.increment(labels: { text: "with \"quotes\", \\escape \n and newline" }, by: 15.0)
+
+
+    qux = registry.summary(:qux,
+                           docstring: 'qux description',
+                           labels: [:for, :code],
+                           preset_labels: { for: 'sake', code: '1' })
+    92.times { qux.observe(0) }
+    qux.observe(1243.21)
+
+
+    xuq = registry.histogram(:xuq,
+                             docstring: 'xuq description',
+                             labels: [:code],
+                             preset_labels: {code: 'ah'},
+                             buckets: [10, 20, 30])
+    xuq.observe(12)
+    xuq.observe(3.2)
   end
 
   describe '.marshal' do
@@ -66,7 +57,7 @@ describe Prometheus::Client::Formats::Text do
 # HELP foo foo description
 foo{umlauts="Björn",utf="佖佥",code="red"} 42.0
 foo{umlauts="Björn",utf="佖佥",code="green"} 3.14e+42
-foo{umlauts="Björn",utf="佖佥",code="blue"} -1.23e-45
+foo{umlauts="Björn",utf="佖佥",code="blue"} 1.23e-45
 # TYPE bar gauge
 # HELP bar bar description\nwith newline
 bar{status="success",code="pink"} 15.0

prometheus/client_ruby

Signed-off-by: Daniel Magliola <danielmagliola@gocardless.com>

spec/prometheus/client/formats/text_spec.rb
@@ -1,62 +1,54 @@
 # encoding: UTF-8
 
+require 'prometheus/client'
+require 'prometheus/client/registry'
 require 'prometheus/client/formats/text'
 
 describe Prometheus::Client::Formats::Text do
-  let(:summary_value) do
-    { "count" => 93.0, "sum" => 1243.21 }
+  # Reset the data store
+  before do
+    Prometheus::Client.config.data_store = Prometheus::Client::DataStores::Synchronized.new
   end
 
-  let(:histogram_value) do
-    { 10 => 1.0, 20 => 2.0, 30 => 2.0, "+Inf" => 2.0, "sum" => 15.2 }
-  end
+  let(:registry) { Prometheus::Client::Registry.new }
 
-  let(:registry) do
-    metrics = [
-      double(
-        name: :foo,
-        docstring: 'foo description',
-        type: :counter,
-        values: {
-          { umlauts: 'Björn', utf: '佖佥', code: 'red' }   => 42.0,
-          { umlauts: 'Björn', utf: '佖佥', code: 'green' } => 3.14E42,
-          { umlauts: 'Björn', utf: '佖佥', code: 'blue' }  => -1.23e-45,
-        },
-      ),
-      double(
-        name: :bar,
-        docstring: "bar description\nwith newline",
-        type: :gauge,
-        values: {
-          { status: 'success', code: 'pink' } => 15.0,
-        },
-      ),
-      double(
-        name: :baz,
-        docstring: 'baz "description" \\escaping',
-        type: :counter,
-        values: {
-          { text: "with \"quotes\", \\escape \n and newline" } => 15.0,
-        },
-      ),
-      double(
-        name: :qux,
-        docstring: 'qux description',
-        type: :summary,
-        values: {
-          { for: 'sake', code: '1' } => summary_value,
-        },
-      ),
-      double(
-        name: :xuq,
-        docstring: 'xuq description',
-        type: :histogram,
-        values: {
-          { code: 'ah' } => histogram_value,
-        },
-      ),
-    ]
-    double(metrics: metrics)
+  before do
+    foo = registry.counter(:foo,
+                           docstring: 'foo description',
+                           labels: [:umlauts, :utf, :code],
+                           preset_labels: {umlauts: 'Björn', utf: '佖佥'})
+    foo.increment(labels: { code: 'red'}, by: 42)
+    foo.increment(labels: { code: 'green'}, by: 3.14E42)
+    foo.increment(labels: { code: 'blue'}, by: 1.23e-45)
+
+
+    bar = registry.gauge(:bar,
+                         docstring: "bar description\nwith newline",
+                         labels: [:status, :code])
+    bar.set(15, labels: { status: 'success', code: 'pink'})
+
+
+    baz = registry.counter(:baz,
+                           docstring: 'baz "description" \\escaping',
+                           labels: [:text])
+    baz.increment(labels: { text: "with \"quotes\", \\escape \n and newline" }, by: 15.0)
+
+
+    qux = registry.summary(:qux,
+                           docstring: 'qux description',
+                           labels: [:for, :code],
+                           preset_labels: { for: 'sake', code: '1' })
+    92.times { qux.observe(0) }
+    qux.observe(1243.21)
+
+
+    xuq = registry.histogram(:xuq,
+                             docstring: 'xuq description',
+                             labels: [:code],
+                             preset_labels: {code: 'ah'},
+                             buckets: [10, 20, 30])
+    xuq.observe(12)
+    xuq.observe(3.2)
   end
 
   describe '.marshal' do
@@ -66,7 +58,7 @@ describe Prometheus::Client::Formats::Text do
 # HELP foo foo description
 foo{umlauts="Björn",utf="佖佥",code="red"} 42.0
 foo{umlauts="Björn",utf="佖佥",code="green"} 3.14e+42
-foo{umlauts="Björn",utf="佖佥",code="blue"} -1.23e-45
+foo{umlauts="Björn",utf="佖佥",code="blue"} 1.23e-45
 # TYPE bar gauge
 # HELP bar bar description\nwith newline
 bar{status="success",code="pink"} 15.0