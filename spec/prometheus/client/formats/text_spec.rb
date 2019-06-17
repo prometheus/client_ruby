394c8df8f4e485de3c9a0219f0bc2b9ecbc26b91
Lawrence Jones <lawrjone@gmail.com>
2019-06-17 09:31:15 +0100

    rubocop: auto-correct Layout/IndentHeredoc

--- spec/prometheus/client/formats/text_spec.rb
+++ spec/prometheus/client/formats/text_spec.rb
@@ -49,30 +49,30 @@ describe Prometheus::Client::Formats::Text do
 
   describe ".marshal" do
     it "returns a Text format version 0.0.4 compatible representation" do
-      expect(subject.marshal(registry)).to eql <<-'TEXT'
-# TYPE foo counter
-# HELP foo foo description
-foo{umlauts="Björn",utf="佖佥",code="red"} 42.0
-foo{umlauts="Björn",utf="佖佥",code="green"} 3.14e+42
-foo{umlauts="Björn",utf="佖佥",code="blue"} 1.23e-45
-# TYPE bar gauge
-# HELP bar bar description\nwith newline
-bar{status="success",code="pink"} 15.0
-# TYPE baz counter
-# HELP baz baz "description" \\escaping
-baz{text="with \"quotes\", \\escape \n and newline"} 15.0
-# TYPE qux summary
-# HELP qux qux description
-qux_sum{for="sake",code="1"} 1243.21
-qux_count{for="sake",code="1"} 93.0
-# TYPE xuq histogram
-# HELP xuq xuq description
-xuq_bucket{code="ah",le="10"} 1.0
-xuq_bucket{code="ah",le="20"} 2.0
-xuq_bucket{code="ah",le="30"} 2.0
-xuq_bucket{code="ah",le="+Inf"} 2.0
-xuq_sum{code="ah"} 15.2
-xuq_count{code="ah"} 2.0
+      expect(subject.marshal(registry)).to eql <<~'TEXT'
+        # TYPE foo counter
+        # HELP foo foo description
+        foo{umlauts="Björn",utf="佖佥",code="red"} 42.0
+        foo{umlauts="Björn",utf="佖佥",code="green"} 3.14e+42
+        foo{umlauts="Björn",utf="佖佥",code="blue"} 1.23e-45
+        # TYPE bar gauge
+        # HELP bar bar description\nwith newline
+        bar{status="success",code="pink"} 15.0
+        # TYPE baz counter
+        # HELP baz baz "description" \\escaping
+        baz{text="with \"quotes\", \\escape \n and newline"} 15.0
+        # TYPE qux summary
+        # HELP qux qux description
+        qux_sum{for="sake",code="1"} 1243.21
+        qux_count{for="sake",code="1"} 93.0
+        # TYPE xuq histogram
+        # HELP xuq xuq description
+        xuq_bucket{code="ah",le="10"} 1.0
+        xuq_bucket{code="ah",le="20"} 2.0
+        xuq_bucket{code="ah",le="30"} 2.0
+        xuq_bucket{code="ah",le="+Inf"} 2.0
+        xuq_sum{code="ah"} 15.2
+        xuq_count{code="ah"} 2.0
       TEXT
     end
   end
